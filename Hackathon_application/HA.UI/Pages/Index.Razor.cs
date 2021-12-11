9fb9cbfb64bc6e22fde02db99bf6d08a153c0f45
PiotrWalczak2001 <01.piotrwalczak@gmail.com>
2021-12-11 13:24:42 +0100

    Update Index.Razor.cs

--- Hackathon_application/HA.UI/Pages/Index.Razor.cs
+++ Hackathon_application/HA.UI/Pages/Index.Razor.cs
@@ -22,10 +22,6 @@ namespace HA.UI.Pages
         protected async override Task OnInitializedAsync()
         {
             Zones = (await ZoneService.GetAllZones()).ToList();
-            foreach (var zone in )
-            {
-
-            }
         }
 
         protected async Task LoadData()
